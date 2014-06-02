e86c4be0e6d7130a38c856c3fd3ace7310ed22f9
John Robinson <johnrobinsn@gmail.com>
2014-06-02 19:32:17 -0400

    Another OSX mixup

--- xs/xs_posix_emu.h
+++ xs/xs_posix_emu.h
@@ -361,15 +361,13 @@ static int xs_poll(struct pollfd *pfdin, int ntotal, int milliseconds) {
 //   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //   THE SOFTWARE.
 // =================================================================================================================
-#if (defined WIN32 || !defined CLOCK_MONOTONIC) && !defined (_xs_TIMECLOCK_H_)
+#if !defined(_STRUCT_TIMESPEC) && (defined WIN32 || !defined CLOCK_MONOTONIC) && !defined (_xs_TIMECLOCK_H_)
 #define _xs_TIMECLOCK_H_
 
-#if 0
 struct timespec {
     time_t   tv_sec;        /* seconds */
     long     tv_nsec;       /* nanoseconds */
 };
-#endif
 
 typedef int clockid_t;
 #define CLOCK_MONOTONIC (1)
